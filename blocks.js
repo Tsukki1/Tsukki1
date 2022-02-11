d91afce6f4f0f7ff9bf9408f4da191565420b98c
Tsukki1 <99043109+Tsukki1@users.noreply.github.com>
2022-02-11 09:50:41 -0800

    Create blocks.js

---
+++ blocks.js
@@ -0,0 +1,28 @@
+blocks.onBlockBroken( GRASS, function () {
+
+})
+
+player.say("Placing Stone")
+blocks.place(BEDROCK, pos(0, 0, 1))
+blocks.place(BEDROCK, pos(0, 0, 2))
+blocks.place(BEDROCK, pos(0, 0, 3))
+blocks.place(BEDROCK, pos(0, 0, 4))
+blocks.place(BEDROCK, pos(0, 0, 5))
+
+blocks.place(BEDROCK, pos(-2, 0, 1))
+blocks.place(BEDROCK, pos(-2, 0, 2))
+blocks.place(BEDROCK, pos(-2, 0, 3))
+blocks.place(BEDROCK, pos(-2, 0, 4))
+blocks.place(BEDROCK, pos(-2, 0, 5))
+
+blocks.place(BEDROCK, pos(-2, 0, 1))
+blocks.place(BEDROCK, pos(-2, 0, 2))
+blocks.place(BEDROCK, pos(-2, 0, 3))
+blocks.place(BEDROCK, pos(-2, 0, 4))
+blocks.place(BEDROCK, pos(-2, 0, 5))
+
+blocks.place(BEDROCK, pos(-2, 0, 1))
+blocks.place(BEDROCK, pos(-2, 0, 2))
+blocks.place(BEDROCK, pos(-2, 0, 3))
+blocks.place(BEDROCK, pos(-2, 0, 4))
+blocks.place(BEDROCK, pos(-2, 0, 5))
